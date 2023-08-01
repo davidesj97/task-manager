75aab63f204e09adea22b66db3ececb58517a6ca
David Eduardo <davidesj97@gmail.com>
2023-07-31 22:52:20 -0600

    Mostrar lista de tareas creadas

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+let tareas = [];
+let contadorId = 0;
 function EnviarTarea() {
-    alert("Tarea enviada")
+    inputTarea = document.getElementById("inputTarea");
+    tareas = [
+        ...tareas,
+        {
+            id: contadorId + 1,
+            texto: inputTarea.value,
+            completada: false,
+        }
+    ]
+    contadorId++;
+    inputTarea.value = "";
+    CrearElemento()
+}
+
+function CrearElemento() {
+    const ulElement = document.querySelector('ul');
+
+    // Paso 2: Eliminar todos los li
+    while (ulElement.firstChild) {
+        ulElement.removeChild(ulElement.firstChild);
+    }
+    for(let i = 0; i < tareas.length; i++) {
+        let li = document.createElement('li');
+        let p = document.createElement('p');
+        let imgCompletar = document.createElement('img');
+        let imgEliminar = document.createElement('img');
+        imgCompletar.src = 'imagenes2/icons8-tick-30.png'
+        imgEliminar.src = 'imagenes2/icons8-trash-30.png'
+        li.setAttribute("id", tareas[i].id);
+        li.setAttribute("class", "tarea");
+        p.setAttribute("class", "texto-tarea")
+        imgCompletar.setAttribute("class", "icon-completar");
+        imgEliminar.setAttribute("class", "icon-eliminar");
+        p.appendChild(document.createTextNode(tareas[i].texto));
+        ulElement.appendChild(li).appendChild(p);
+        ulElement.appendChild(li).appendChild(imgCompletar);
+        ulElement.appendChild(li).appendChild(imgEliminar);
+    }
 }
