433b452bf3aced1a59b02e9ecee99f0ab3b88dd3
David Eduardo <davidesj97@gmail.com>
2023-08-01 22:35:04 -0600

    Agregar contador para tareas completadas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let tareas = [];
 let contadorId = 0;
+
 function EnviarTarea() {
     inputTarea = document.getElementById("inputTarea");
     tareas = [
@@ -16,32 +17,55 @@ function EnviarTarea() {
     contadorTareasTotales();
 }
 
+function CompletarTarea(id) {
+    const tarea = tareas.find(t => t.id === id);
+    if (tarea) {
+        tarea.completada = true;
+        CrearElemento();
+        contadorTareasCompletadas();
+    }
+}
+
 function CrearElemento() {
     const ulElement = document.querySelector('ul');
     while (ulElement.firstChild) {
         ulElement.removeChild(ulElement.firstChild);
     }
-    for(let i = 0; i < tareas.length; i++) {
+    tareas.forEach(tarea => {
         let li = document.createElement('li');
         let p = document.createElement('p');
         let imgCompletar = document.createElement('img');
         let imgEliminar = document.createElement('img');
         imgCompletar.src = 'imagenes2/icons8-tick-30.png'
+        imgCompletar.addEventListener("click", () => CompletarTarea(tarea.id))
         imgEliminar.src = 'imagenes2/icons8-trash-30.png'
-        li.setAttribute("id", tareas[i].id);
+
+        li.setAttribute("id", tarea.id);
         li.setAttribute("class", "tarea");
         p.setAttribute("class", "texto-tarea")
         imgCompletar.setAttribute("class", "icon-completar");
         imgEliminar.setAttribute("class", "icon-eliminar");
-        p.appendChild(document.createTextNode(tareas[i].texto));
+
+        p.appendChild(document.createTextNode(tarea.texto));
         ulElement.appendChild(li).appendChild(p);
         ulElement.appendChild(li).appendChild(imgCompletar);
         ulElement.appendChild(li).appendChild(imgEliminar);
-    }
+    })
 }
 
 function contadorTareasTotales() {
     const tareasTotales = tareas.length;
-    elementoTareasTotales = document.getElementById("tareas-totales");
+    let elementoTareasTotales = document.getElementById("tareas-totales");
     elementoTareasTotales.innerText = tareasTotales;
+}
+
+function contadorTareasCompletadas() {
+    let tareasCompletadas = 0;
+    let elementoTareasCompletadas = document.getElementById("tarea-compl");
+    tareas.forEach(tarea => {
+        if(tarea.completada) {
+            tareasCompletadas++;
+        }
+    })
+    elementoTareasCompletadas.innerText = tareasCompletadas
 }
