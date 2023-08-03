8dea3a2a23a2074b0a584aba0aaf27abb16ad215
David Eduardo <davidesj97@gmail.com>
2023-08-02 21:58:30 -0600

    Agregar estilos al boton de completar tarea

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let tareas = [];
+let tareasCreadas = [];
+let tareasCompletas = [];
 let contadorId = 0;
 
 function EnviarTarea() {
     inputTarea = document.getElementById("inputTarea");
-    tareas = [
-        ...tareas,
+    tareasCreadas = [
+        ...tareasCreadas,
         {
             id: contadorId + 1,
             texto: inputTarea.value,
@@ -18,7 +19,7 @@ function EnviarTarea() {
 }
 
 function CompletarTarea(id) {
-    const tarea = tareas.find(t => t.id === id);
+    const tarea = tareasCreadas.find(t => t.id === id);
     if (tarea) {
         tarea.completada = true;
         CrearElemento();
@@ -31,30 +32,36 @@ function CrearElemento() {
     while (ulElement.firstChild) {
         ulElement.removeChild(ulElement.firstChild);
     }
-    tareas.forEach(tarea => {
-        let li = document.createElement('li');
-        let p = document.createElement('p');
-        let imgCompletar = document.createElement('img');
-        let imgEliminar = document.createElement('img');
-        imgCompletar.src = 'imagenes2/icons8-tick-30.png'
-        imgCompletar.addEventListener("click", () => CompletarTarea(tarea.id))
-        imgEliminar.src = 'imagenes2/icons8-trash-30.png'
-
-        li.setAttribute("id", tarea.id);
-        li.setAttribute("class", "tarea");
-        p.setAttribute("class", "texto-tarea")
-        imgCompletar.setAttribute("class", "icon-completar");
-        imgEliminar.setAttribute("class", "icon-eliminar");
-
-        p.appendChild(document.createTextNode(tarea.texto));
-        ulElement.appendChild(li).appendChild(p);
-        ulElement.appendChild(li).appendChild(imgCompletar);
-        ulElement.appendChild(li).appendChild(imgEliminar);
+    tareasCreadas.forEach(tarea => {
+        if(!tarea.completada) {
+            let li = document.createElement('li');
+            let p = document.createElement('p');
+            let imgCompletar = document.createElement('img');
+            let imgEliminar = document.createElement('img');
+            let botonCompletar = document.createElement('div')
+            let botonEliminar = document.createElement('div')
+            imgCompletar.src = 'imagenes2/icons8-tick-30.png'
+            imgCompletar.addEventListener("click", () => CompletarTarea(tarea.id))
+            imgEliminar.src = 'imagenes2/icons8-trash-30.png'
+    
+            li.setAttribute("id", tarea.id);
+            li.setAttribute("class", "tarea");
+            p.setAttribute("class", "texto-tarea")
+            botonCompletar.setAttribute("class", "btnCompletar")
+            botonEliminar.setAttribute("class", "btnEliminar")
+            imgCompletar.setAttribute("class", "icon-completar");
+            imgEliminar.setAttribute("class", "icon-eliminar");
+    
+            p.appendChild(document.createTextNode(tarea.texto));
+            ulElement.appendChild(li).appendChild(p);
+            ulElement.appendChild(li).appendChild(botonCompletar).appendChild(imgCompletar);
+            ulElement.appendChild(li).appendChild(botonEliminar).appendChild(imgEliminar);
+        }
     })
 }
 
 function contadorTareasTotales() {
-    const tareasTotales = tareas.length;
+    const tareasTotales = tareasCreadas.length;
     let elementoTareasTotales = document.getElementById("tareas-totales");
     elementoTareasTotales.innerText = tareasTotales;
 }
@@ -62,10 +69,22 @@ function contadorTareasTotales() {
 function contadorTareasCompletadas() {
     let tareasCompletadas = 0;
     let elementoTareasCompletadas = document.getElementById("tarea-compl");
-    tareas.forEach(tarea => {
+    tareasCreadas.forEach(tarea => {
         if(tarea.completada) {
             tareasCompletadas++;
         }
     })
     elementoTareasCompletadas.innerText = tareasCompletadas
+}
+
+function listarTareasCompletadas () {
+    let listaTareasCompletas = [];
+    tareasCreadas.forEach(tarea => {
+        if(tarea.completada === true) {
+            listaTareasCompletas = [
+                ...listaTareasCompletas,
+                tarea
+            ]
+        }
+    })
 }
